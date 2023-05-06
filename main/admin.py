eeefef5c6ecf9bb3b7209e71b1bf38f82b5e8eaf
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-05-06 20:57:15 +0300

    add admin panel-2

--- main/admin.py
+++ main/admin.py
@@ -4,11 +4,12 @@ from main.models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price_for_pickup', 'category')
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'price_for_pickup', 'category',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description')
