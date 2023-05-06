ae93813038f23396fe4deae77ff36c3a1f84250c
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-05-06 19:48:47 +0300

    add admin panel

--- dz_20_1/settings.py
+++ dz_20_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project',
         'USER': 'postgres',
-        'PASSWORD': '12345'
+        'PASSWORD': os.getenv('bd_pass')
     }
 }
 
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from main.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_for_pickup', 'category')
+    search_fields = ('name',)
