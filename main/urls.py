e302a6c076c9d1aae3b5c3e082070c94b9ae5ef4
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-05-10 21:49:18 +0300

    ch html

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
+from main.views import index, contacts, products, product
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    # path('', post_route)  # получение запроса из постмана
+    path('contacts/', contacts, name='contacts'),  # Вывод url контакты  из views.py contacts
+    path('products/', products, name='products_list'),
+    path('product/<int:pk>/', product, name='product_item'),  # url студента с переменной, в контроллере(views) так же указывается
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 
+from main.models import Product
+
 
 def index(request):
-    return render(request, 'main/index.html')
+    # ШАБЛОННЫЕ ПЕРЕМЕННЫЕ
+    context = {
+        'object_list': Product.objects.all()[:5],   # выведение контекста студентов из модели по ключу object_list
+        'title': 'Главная'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def products(request):
+    # ШАБЛОННЫЕ ПЕРЕМЕННЫЕ
+    context = {
+        'object_list': Product.objects.all(),   # выведение контекста студентов из модели по ключу object_list
+        'title': 'Все товары'
+    }
+    return render(request, 'main/products.html', context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'objects': product_item,
+        'title': product_item
+    }
+    return render(request, 'main/product.html', context)
+
+
+def contacts(request):  # в файле request хранится вся инфа от пользователя
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'name {name}, email {email}, message {message}')
+
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'main/contact.html', context)  # шаблон main/contact.html
