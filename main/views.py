d1b46d14c8c102359b14a176795ccba43e9652e4
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-05-12 10:47:01 +0300

    add dz 20.2(2)

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from main.models import Product
 def index(request):
     # ШАБЛОННЫЕ ПЕРЕМЕННЫЕ
     context = {
-        'object_list': Product.objects.all()[:3],   # выведение контекста студентов из модели по ключу object_list
+        'object_list': Product.objects.all(),   # выведение контекста студентов из модели по ключу object_list
         'title': 'Главная'
     }
     return render(request, 'main/index.html', context)
