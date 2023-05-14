893fa335a68c7a7a9ebc1079160ea2f9d9d8a9b2
Javoprav <Javoprav@gmail.com>
2023-05-14 22:25:29 +0300

    ch fill-----1

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -1,24 +1,52 @@
 import json, os
 from pprint import pprint
 
+import psycopg2
 from django.core.management import BaseCommand
+
+from config import config
 from main.models import Product, Category
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print('Моя команда')
 
-        Category_list_all = Category.objects.all()
-        Category_list_all.delete()
+        params = config()
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        try:
+            cur.execute(f"DROP DATABASE project")
+        except psycopg2.errors.InvalidCatalogName:
+            print(f'psycopg2.errors.InvalidCatalogName: ОШИБКА:  база данных project не существует')
+        try:
+            cur.execute(f"CREATE DATABASE project")
+        except psycopg2.errors.DuplicateDatabase:
+            print('ОШИБКА:  отношение "main_category" не существует')
+        conn.close()
 
-        product_list_all = Product.objects.all()
-        product_list_all.delete()
+        # if Category.objects.all():
+        #     category_list_all = Category.objects.all()
+        #     category_list_all.delete()
 
-        os.system("python manage.py loaddata category.json")
+        # if Product.objects.all():
+        #     product_list_all = Product.objects.all()
+        #     product_list_all.delete()
 
-        with open('db.json', 'r', encoding='Windows-1251') as f:
+        os.system("python manage.py migrate")
+        # os.system("python manage.py loaddata category.json")   # Windows-1251
+
+        with open('category.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            f.close()
+
+        category_list = []
+        for category in data:
+            category_list.append(Category(**category["fields"]))
+        Category.objects.bulk_create(category_list)
+
+        with open('db.json', 'r', encoding='utf-8') as f:
             data = json.load(f)
             f.close()
 
@@ -29,5 +57,4 @@ class Command(BaseCommand):
         product_list = []
         for product in data:
             product_list.append(Product(**product["fields"]))
-
         Product.objects.bulk_create(product_list)
